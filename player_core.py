5f5f397c5a2d502adb3ceded233cd2ed0f4751a1
jccastr0 <jeremycoloncastro@gmail.com>
2024-03-13 13:19:32 -0700

    art

--- player_core.py
+++ player_core.py
@@ -3,6 +3,34 @@
 
 #import boss_core
 #ascii wizard
+with open ('artwork.txt') as file:
+    print(file.read())
+
+print("\nClasses: \n")
 wizard = print("Wizard: Harnessing the power of magic allows you control over the power of vitality! On critical rolls gain back 10 health.")
 #ascii warrior
-warrior= print("Warrior: Channel your inner rage and conquer any foe with your mighty sword! On critical rolls get a damage boost.")
+warrior= print("\nWarrior: Channel your inner rage and conquer any foe with your mighty sword! On critical rolls get a damage boost.")
+
+run=True 
+
+while run == True:
+
+    class_selection = input("\nWhat class do you choose challenger? ")
+
+    
+    if class_selection.lower() == "warrior":
+        print("\n*POOF*\n")
+        print("A warrior is born and ready for battle!")
+        player_class = "warrior"
+        run = False
+        
+
+    elif class_selection.lower() == "wizard":
+        print("\n*POOF*\n")
+        print("A wizard arises!")
+        player_class = "wizard"
+        run = False
+    else:
+        print("\nInteresting...however "+ class_selection +" was not a choice!")
+
+
