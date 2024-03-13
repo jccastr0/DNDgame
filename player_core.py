7951cfc8b7b4f994d4d4283e886cefd0d39856a9
jccastr0 <jeremycoloncastro@gmail.com>
2024-03-13 11:38:06 -0700

    player

---
+++ player_core.py
@@ -0,0 +1,8 @@
+# wizard = +10 health on crit roll (20)
+# warrior = +5 attack on crit roll (20)
+
+#import boss_core
+#ascii wizard
+wizard = print("Wizard: Harnessing the power of magic allows you control over the power of vitality! On critical rolls gain back 10 health.")
+#ascii warrior
+warrior= print("Warrior: Channel your inner rage and conquer any foe with your mighty sword! On critical rolls get a damage boost.")
