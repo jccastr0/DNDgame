e8fcd17bb6296700466b86299779ff245c09d1ba
jccastr0 <jeremycoloncastro@gmail.com>
2024-03-14 14:06:27 -0700

    changse

--- game_logic2.py
+++ game_logic2.py
@@ -1,19 +1,27 @@
 import random
 
-run = True
+player_health = 100
 
-player_health = 20
+boss_health = 50
 
-boss_health = 20
+while player_health > 0 and boss_health > 0:
 
-while run == True:
+    boss_roll = random.randint(1,20)
 
-    boss_roll = random.randint(1,9)
-
-    player_roll = random.randint(1,9)
+    player_roll = random.randint(1,12)
 
     player_health = player_health - boss_roll
 
-    if player_health <= 0:
-        print("Game Over!")
-    run == False
+    boss_health = boss_health - player_roll
+    
+    print("\nBoss attacks and deals "+ str(boss_roll)+" damage.\nYou now have "+ str(player_health) +" health!")
+
+    print("You did "+ str(player_roll) +" damage!")
+
+    print ("Boss has " + str(boss_health)+" health!\n")
+
+if player_health > boss_health:
+    print("You win!")
+else:
+    print('Foolish mortal...')
+    print("Game Over!")
