2f5cf7e202afc83c4374e98853860893cb64b454
jccastr0 <jeremycoloncastro@gmail.com>
2024-03-15 11:40:01 -0700

    changes

--- Welcome_To_MonsterCavern.py
+++ Welcome_To_MonsterCavern.py
@@ -1,25 +1,30 @@
 
-import asciiART
-import player_core
-import game_logic2
+# import asciiART
+# import player_core
+# import game_logic2
 
-run = True
+def begin():
+    import asciiART
+    import player_core
+    import game_logic2
 
-asciiART.GameScreen()
+    run = True
 
-while run == True:
-    
-    intro_cutscene = input("\nDo you dare challenge the Monster Cavern?\n")
+    asciiART.GameScreen()
 
-    if intro_cutscene.lower() == "yes":
-        print("\nYou will now stare at the darkness of the abyss, but forwarned the abyss stares back...")
-        run = False
-    else:
-        print("\nBegone, you're not worthy anyways. The world was overrun with monsters...\n")
-        run = False
+    while run == True:
+        
+        intro_cutscene = input("\nDo you dare challenge the Monster Cavern?\n")
 
-    if intro_cutscene.lower() == "yes":
-        game_logic2.boss()
+        if intro_cutscene.lower() == "yes":
+            print("\nYou will now stare at the darkness of the abyss, but forwarned the abyss stares back...")
+            run = False
+        else:
+            print("\nBegone, you're not worthy anyways. The world was overrun with monsters...\n")
+            run = False
+
+        if intro_cutscene.lower() == "yes":
+            game_logic2.boss()
 
 
 
